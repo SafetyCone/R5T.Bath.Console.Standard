cafb68a15dd5165881200530fdd826602594ee19
MinexAutomation <minex.automation@gmail.com>
2020-01-19 18:32:16 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Bath.Console.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Bath.Console.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConsoleHumanOutput(this IServiceCollection services)
+        {
+            services.AddSingleton<IHumanOutput, ConsoleHumanOutput>();
+
+            return services;
+        }
+    }
+}
